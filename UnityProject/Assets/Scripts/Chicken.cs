fd2f7a8092f1b4ebb4b6a2f16cb012b6a2e75fad
Shih1210 <55180792+Shih1210@users.noreply.github.com>
2019-10-30 16:00:35 +0800

    角色動畫

---
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;//使用unity API
+
+//類別 類別名稱
+public class Chicken : MonoBehaviour
+{
+    int speed = 10;
+    float turn = 20.5f;
+    bool mission;
+    string Name="Chicken ";
+}
