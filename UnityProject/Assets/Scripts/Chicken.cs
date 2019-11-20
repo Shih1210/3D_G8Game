89d9946311bada332b754e3addd422c7277d855e
Shih1210 <qoo1210@gmail.com>
2019-11-20 16:06:55 +0800

    自訂參數

--- UnityProject/Assets/Scripts/Chicken.cs
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -3,8 +3,17 @@
 //類別 類別名稱
 public class Chicken : MonoBehaviour
 {
-    int speed = 10;
-    float turn = 20.5f;
-    bool mission;
-    string Name="Chicken ";
+    //宣告變數(定義欄位 Field)
+    //修飾詞 欄位類型 欄位名稱(指定 數值)結束；
+    //私人 Private ；公開 Public
+    [Header("移動速度")][Range(1,100)]
+    public int speed = 10;
+    [Header("移動速度")]
+    [Tooltip("雞的旋轉速度"),Range(1.5f,200f)]
+    //說明文字 Range(最小值,最大值)→拉桿
+    public float turn = 20.5f;
+    [Header("是否完成任務")]
+    public bool mission;
+    [Header("玩家名稱")]
+    public string Name ="Chicken ";
 }
---
+++ UnityProject/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //定義方法
+    //語法
+    //修飾詞 傳回類型 方法名稱(參數)[陳述式或演算法]
+    //參數類型 參數名稱 參數:方便後續維護
+    //void 無傳回
+    //自訂需要被呼叫
+    /// <summary>
+    /// 開車方法
+    /// </summary>
+    /// <param name="speed">開車速度</param>
+    private void Drive(int speed)
+    {
+        //輸出訊息(字串);
+        print("音效~");
+        //可使用+符號連接字串與其他類型
+        print("開車，時速:" + speed);
+    }
+
+    //有預設值的參數 選填式參數 呼叫時可不填寫
+    //選填式參數只能寫在最右邊
+    private void shoot(int count, float speed, string prop = "無",string direction = "前方") 
+    {
+        print("弓箭數量:"+count);
+        print("弓箭速度:"+speed);
+        print("弓箭屬性:"+prop);
+        print("弓箭方向:"+direction);
+    }
+
+    //事件:在指定的時間會以指定次數執行的方法
+    //start 初始事件:遊戲播放後執行一次
+    private void Start()
+    {
+        print("哈囉，沃的~");
+
+        //自訂的呼叫方式 寫幾次撥放幾次
+        Drive(200);
+        Drive(300);
+
+        shoot(1, 1.5f);
+        shoot(10, 10.5f,"火屬性");
+        shoot(3,2,direction:"前後方");
+    }
+}
