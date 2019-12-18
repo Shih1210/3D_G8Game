21c6fb273fe654cef2ff42e41ef765b3f1994e5e
Shih1210 <55180792+Shih1210@users.noreply.github.com>
2019-12-18 16:01:13 +0800

    Lerp.攝影機移動

--- UnityProject/Assets/Scripts/Chicken.cs
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -22,6 +22,9 @@ public class Chicken : MonoBehaviour
     public Transform tran;
     public Rigidbody rig;
     public Animator ani;
+    public AudioSource aud;
+
+    public AudioClip soundBark;
 
     private void Update()
     {
@@ -37,12 +40,17 @@ public class Chicken : MonoBehaviour
     ///</summary>
     private void Run()
     {
+        //如果動畫為 撿東西 就 跳出
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("撿東西")) return;
+
         float v = Input.GetAxis("Vertical");    //W上 1，S下 -1 ，沒有 0
         //rig.AddForce(0, 0, speed * v);                //世界座標
         //tran.forward 區域座標Z軸
         //tran.right   區域座標X軸
         //tran.up      區域座標Y軸
         rig.AddForce(tran.forward * speed * v*Time.deltaTime);       //區域座標
+
+        ani.SetBool("走路開關", v != 0);
     }
     ///<summary>
     ///旋轉
@@ -57,17 +65,25 @@ public class Chicken : MonoBehaviour
     ///</summary>
     private void Bark()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //按下空白鍵拍翅膀
+            ani.SetTrigger("拍翅膀觸發器");
 
+            aud.PlayOneShot(soundBark,0.6f);
+
+        }
     }
     ///<summary>
     ///撿東西
     ///</summary>
     private void Catch()
     {
-        Animator 撿東西;
-        float 撿東西觸發器;
-
-
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+                //按下左鍵撿東西
+              ani.SetTrigger("撿東西觸發器");
+        }
     }
     ///<summary>
     ///檢視任務
